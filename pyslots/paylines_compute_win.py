2d7d8db98f435bbf9358a2368f2567b500d99160
frank <fanzc@126.com>
2023-02-07 11:03:54 +0800

    继续完善计算赔率的测试用例

--- pyslots/paylines_compute_win.py
+++ pyslots/paylines_compute_win.py
@@ -12,6 +12,10 @@ import pandas
 DEBUG_ON = False
 PACKAGE_NAME = 'paylines_compute_win'
 
+def pprint(str):
+    if DEBUG_ON:
+        print(str)
+
 # read pl and gl from excel
 def get_pl_from_excel(excel_file_name):
     pl_list = []
@@ -23,24 +27,24 @@ def get_pl_from_excel(excel_file_name):
     pl_need_num_list = data.values[:, 4].tolist()
     sum_gl = sum(gl_list)
     if sum_gl > 1 or sum_gl < 0.5:
-        if DEBUG_ON:
-            print(PACKAGE_NAME +": the sum of gl is wrong, result: " + str(sum_gl))
+        pprint("****package:" + PACKAGE_NAME + "  ****funtion:get_pl_from_excel, total peilv is:" + str(sum_gl))
     return pl_list, gl_list, pl_need_num_list
 
-TUAN_PL_MAP = {
-    '9' :[0, 0, 0, 2, 5],
-    '10':[0, 0, 0, 5, 10],
-    'J' :[0, 0, 2, 10, 15],
-    'Q' :[0, 0, 0, 15, 20],
-    'K' :[0, 0, 5, 20, 30],
-    'A': [0, 0, 10, 25, 40],
-    'W': [0, 0, 15, 30, 50],
-    'S': [0, 0, 0, 2, 5],
-}
+# 以下数据只是为了说明数据格式，举例用的
+# TUAN_PL_MAP = {
+#     '9' :[0, 0, 0, 2, 5],
+#     '10':[0, 0, 0, 5, 10],
+#     'J' :[0, 0, 2, 10, 15],
+#     'Q' :[0, 0, 0, 15, 20],
+#     'K' :[0, 0, 5, 20, 30],
+#     'A': [0, 0, 10, 25, 40],
+#     'W': [0, 0, 15, 30, 50],
+#     'S': [0, 0, 0, 2, 5],
+# }
 # for pay lines games only, compute the odd of actual single pay line
 # the first tuan can't be 'W'
 # single_pay_line_list = ['9', 'W', '9', '9', 'W']
-# 返回3个参数:第1个图案，连续图案数，这条payline的中奖金额
+# 返回3个参数:这条payline的第1个图案，连续图案数，这条payline的中奖金额
 # private方法，仅供get_result_for_all_tuan调用
 def compute_win_for_single_payline(single_pay_line_list, pl_map):
     lianxu_tuan_nums = 0    # 连续非'W'图案的个数
@@ -55,9 +59,9 @@ def compute_win_for_single_payline(single_pay_line_list, pl_map):
             lianxu_W_nums = lianxu_W_nums + 1
         else:
             break
-    if DEBUG_ON:
-        print(str(lianxu_tuan_nums))
-        print(str(lianxu_W_nums))
+    pprint("****package:" + PACKAGE_NAME + "  ****funtion:compute_win_for_single_payline, 连续普通图案数量 is:" + str(lianxu_tuan_nums))
+    pprint("****package:" + PACKAGE_NAME + "  ****funtion:compute_win_for_single_payline, 连续Wild图案数量 is:" + str(
+        lianxu_tuan_nums))
     tuan = ''
     tuan_jiangjin = 0
     if lianxu_W_nums == lianxu_tuan_nums:
@@ -69,13 +73,14 @@ def compute_win_for_single_payline(single_pay_line_list, pl_map):
     return [tuan, lianxu_tuan_nums, jiangjin]
 
 ######################################################
-ALL_TUAN = [['9','J','9','9','J'],
-            ['Q','10','K','A','S'],
-            ['Q','W','Q','9','10']]
-PAYLINES = [[(0,0), (2,1), (0,2), (2,3), (2,4)],
-            [(2,0), (0,1), (1,2), (2,3), (2,4)],
-            [(1,0), (2,1), (2,2), (2,3), (2,4)],
-            [(0,0), (0,1), (1,2), (2,3), (2,4)]]
+# 以下数据只是为了说明数据格式，举例用的
+# ALL_TUAN = [['9','J','9','9','J'],
+#             ['Q','10','K','A','S'],
+#             ['Q','W','Q','9','10']]
+# PAYLINES = [[(0,0), (2,1), (0,2), (2,3), (2,4)],
+#             [(2,0), (0,1), (1,2), (2,3), (2,4)],
+#             [(1,0), (2,1), (2,2), (2,3), (2,4)],
+#             [(0,0), (0,1), (1,2), (2,3), (2,4)]]
 # private方法，仅供get_result_for_all_tuan调用
 # 根据所有图案和单条支付线，生成单条支付线的图案调用
 # 返回结果: ['9', 'W', '9', '9', 'W']
@@ -102,16 +107,61 @@ def compute_win_for_all_tuan(all_tuan, paylines, pl_map):
             total_odd = total_odd + singleline_result[2]
     return [result,total_odd]
 
+########################  以下是单元测试用到的代码 ########################
+########################           测试用例1    ########################
+UNIT_TEST_ALL_TUAN_LIST = [ ['9','9','9','9','J'],
+                            ['Q','Q','W','A','S'],
+                            ['A','W','A','A','10'],
+                            ['9', 'Q', 'W', 'A', 'S'],
+                            ['S', 'S', 'S', 'A', '10'],
+                            ]
+
+UNIT_TEST_TUAN_PL_MAP = {
+                        '9' :[0, 0, 0, 2, 5],
+                        '10':[0, 0, 0, 5, 10],
+                        'J' :[0, 0, 2, 10, 15],
+                        'Q' :[0, 0, 10, 15, 20],
+                        'K' :[0, 0, 5, 20, 30],
+                        'A': [0, 0, 10, 25, 40],
+                        'W': [0, 0, 15, 30, 50],
+                        'S': [0, 0, 0, 2, 5],}
+UNIT_TEST_RESULT_LIST = [['9', 4, 2], ['Q', 3, 10],['A', 4, 25],['9', 1, 0],['S', 3, 0]]
+
+def UNIT_TEST_compute_win_for_single_payline(tuan_list, pl_map, result_list):
+    for i in range(len(result_list)):
+        res = compute_win_for_single_payline(tuan_list[i], pl_map)
+        assert res ==  result_list[i]
+
+########################           测试用例2    ########################
+UNIT_TEST_TUAN1 = [['K', '10', '9', 'J', '10'], ['9', 'W', 'Q', '9', 'K'], ['J', 'A', 'K', 'Q', 'S']]
+# 每条支付线对应的矩阵坐标位置
+UNIT_TEST_PAYLINES1 = [[(0, 0), (1, 1), (2, 2), (1, 3), (0, 4)],
+                     [(2, 0), (1, 1), (0, 2), (1, 3), (2, 4)],
+                     [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4)],
+                     [(0, 0), (1, 1), (2, 2), (2, 3), (2, 4)],
+                     [(2, 0), (2, 1), (1, 2), (2, 3), (2, 4)]]
+UNIT_TEST_WIN_RESUT1 = [[['K', 3, 5, 0], ['K', 3, 5, 3]], 10]
+
+UNIT_TEST_TUAN2 = [['10', 'J', 'W', 'K', 'A', 'W'], ['9', 'S', 'K', 'Q', 'J',  'Q'],
+                   ['J', 'W', 'Q', '9', '10', 'J'], ['Q', '9', 'J', 'K', '10',  'A']]
+# 每条支付线对应的矩阵坐标位置
+UNIT_TEST_PAYLINES2 = [ [(1, 0), (3, 1), (0, 2), (3, 3), (0, 4), (2, 5)],
+                        [(1, 0), (3, 1), (0, 2), (2, 3), (1, 4), (3, 5)],
+                        [(2, 0), (0, 1), (0, 2), (2, 3), (0, 4), (2, 5)],
+                        [(2, 0), (0, 1), (1, 2), (2, 3), (1, 4), (2, 5)],]
+UNIT_TEST_WIN_RESUT2 = [[['9', 4, 2, 1], ['J', 3, 2, 2]], 4]
+def UNIT_TEST_compute_win_for_all_tuan(all_tuan, paylines, pl_map, result):
+    res = compute_win_for_all_tuan(all_tuan, paylines, pl_map)
+    assert res == result
+
 if __name__ == '__main__':
+
+    print("****package:" + PACKAGE_NAME + "  ****funtion:main, 测试用例1")
+    UNIT_TEST_compute_win_for_single_payline(UNIT_TEST_ALL_TUAN_LIST, UNIT_TEST_TUAN_PL_MAP, UNIT_TEST_RESULT_LIST)
+    print("****package:" + PACKAGE_NAME + "  ****funtion:main, 测试用例2")
+    UNIT_TEST_compute_win_for_all_tuan(UNIT_TEST_TUAN1, UNIT_TEST_PAYLINES1, UNIT_TEST_TUAN_PL_MAP, UNIT_TEST_WIN_RESUT1)
+    UNIT_TEST_compute_win_for_all_tuan(UNIT_TEST_TUAN2, UNIT_TEST_PAYLINES2, UNIT_TEST_TUAN_PL_MAP,
+                                       UNIT_TEST_WIN_RESUT2)
+    # 测试读取excel
     EXCEL_NAME_READ_PV = r"d:py\\1.xlsx"
-    singlelist = ['9','9','9','9','10']
-    singlelist = ['9', 'W', '9', '9', 'W']
-    r = compute_win_for_single_payline(singlelist, TUAN_PL_MAP)
-    #print(str(r))
-
-    r = get_single_payline_from_all_tuan(ALL_TUAN, PAYLINES[0])
-    #print(str(r))
-
-    r = compute_win_for_all_tuan(ALL_TUAN, PAYLINES, TUAN_PL_MAP)
-    print(str(r))
     # get_pl_from_excel(EXCEL_NAME_READ_PV)
